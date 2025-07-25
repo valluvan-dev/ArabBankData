b1264638acec2fe11f9df17bc7a3ec398580e951
valluvan-dev <btreerepo.pythonfsd@gmail.com>
2025-07-25 11:39:32 +0530

    "Reordered path in marketplace/urls.py"

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('ccii/',instant_market_place,name="ccii"),
     path('sales/',sales_view,name="sales"),
     path('get_sample_data/', get_sample_data, name='get_sample_data'),
-    # path('custom_query/', custom_query, name='custom_query'),
+    path('custom_query/', custom_query, name='custom_query'),
 ]
